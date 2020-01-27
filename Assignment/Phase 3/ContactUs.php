36a921ac90a1ca86b52bb3d27fb7fc1833ead66d
saifkh122 <saif.khatatbeh@htu.edu.jo>
2020-01-27 07:47:21 +0200

    Assignment done

--- Assignment/Phase 3/ContactUs.php
+++ Assignment/Phase 3/ContactUs.php
@@ -1,18 +1,14 @@
-<?php 
-    $name = $_POST['name'];
-    $visitor_email = $_POSTp['email'];
-    $message = $_POST['message'];
+<?php
+$to = "saif-kh133@hotmail.com";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-    $email_form = 'saif.khatatbeh@htu.edu.jo';
-    $email_subject = "new form submision";
-    $email_body = "User Name = $name.\n".
-                  "User Email = $visitor_email.\n".
-                  "User Message = $message.\n";
-    
-    $to = "saif-kh133@hotmail.com";
-    $headers = "From: $email_form \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
-    mail($to,$email_subject,$email_body,$headers);
-    header("Location: ContactUs.html");
+  if (mail($to, $email, $name, $phone, $message)) {
+    echo file_get_contents("ContactUs.html");
+  }
+  else {
+    echo "failed";
+  }
 
 ?>
